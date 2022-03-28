e4b5936498294e6212512331a7115fe30a634695
wgabriel14 <16-10973@usb.ve>
2022-03-28 18:31:53 -0400

    old_people new list was created. It allows you to know if a person is old >70

--- data_filter.py
+++ data_filter.py
@@ -77,12 +77,18 @@ DATA = [
 
 def run():
     all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"]== "Platzi"]
-    
+    all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"]== "Platzi"]    
     adults = list(filter(lambda worker: worker["age"] > 18, DATA))
     adults = list(map(lambda worker: worker["name"], adults))
 
-    for worker in adults:
+
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+    """
+    old_people only works in python 3.9 or new versions
+    '|' allows you to add dicts
+    """
+
+    for worker in old_people:
         print(worker)
 
 
