2c7cba4b5e943f9a1a52c974ba9dcc097e0f4761
wgabriel14 <16-10973@usb.ve>
2022-03-28 18:11:31 -0400

    adults filter (age>18) was built and is ready to be used

--- data_filter.py
+++ data_filter.py
@@ -78,7 +78,11 @@ DATA = [
 def run():
     all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
     all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"]== "Platzi"]
-    for worker in all_platzi_workers:
+    
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    adults = list(map(lambda worker: worker["name"], adults))
+
+    for worker in adults:
         print(worker)
 
 
